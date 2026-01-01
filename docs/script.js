71c39aa7fd8521d013e11d89e0612fbdd584978f
Sepehr <spr.sepehr94@gmail.com>
2026-01-01 22:48:37 +0330

    testing new css for phones attempt #1

--- docs/script.js
+++ docs/script.js
@@ -14,27 +14,30 @@ function addOrRemoveNotes(action){
         return;
     }else if(action === "add"){
         // child elements added
-        const paragElem = document.createElement("p");
+        const paragElem = document.createElement("div");
         const delXElem = document.createElement("button");
         // text for elements
         paragElem.innerHTML = userNote;
         delXElem.textContent = "X";
         // element style
+        delXElem.classList.add("delete-note");
+        paragElem.classList.add("sticky-note");
         paragElem.style.whiteSpace = "pre-wrap";
+        // paragElem.style.border = "2px solid red"
         delXElem.style.color = "red";
         // config for delXElem button
         delXElem.addEventListener("click", () => {
+            delXElem.parentElement.remove();
             delXElem.previousSibling.remove();
             delXElem.remove();
         })
         // append elements
         olist.appendChild(paragElem);
-        olist.appendChild(delXElem);
+        paragElem.appendChild(delXElem);
         // clear editor
         tinymce.get("textInput").setContent("")
         tinymce.get("textInput").focus()
     }else if(action === "remove"){
-        olist.lastChild.previousSibling.remove()
         olist.lastChild.remove()
     }
     // 2. End the timer and measure the difference
@@ -50,36 +53,49 @@ addButton.addEventListener("click", () => {
     addOrRemoveNotes("add");
 });
 deleteButton.addEventListener("click", () => {
-    const lastChild = olist.lastChild;
-    const sibling = lastChild.previousSibling;
-    sibling.remove();
-    lastChild.remove();
+      let child = olist.lastChild
+      if(child != null){
+        child.remove();
+      }else {
+        return;
+      }
 });
 
 tinymce.init({
   selector: '#textInput',
-//   height: 300,
-//   with: 200,
-
   license_key: 'gpl',
 
-  plugins: [
-    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
-    'preview', 'anchor', 'searchreplace', 'visualblocks',
-    'code', 'fullscreen', 'insertdatetime', 'media',
-    'table', 'help', 'wordcount'
-  ],
+  height: 45,
+  resize: false,
 
-  toolbar:
-    'undo redo | blocks | ' +
-    'bold italic underline strikethrough | forecolor backcolor | ' +
-    'bullist numlist outdent indent | ' +
-    'removeformat | link image media table | ',
 
-  menubar: true,
+  selector: 'textarea',
+  skin: 'oxide-dark',
+  // content_css: 'dark',
+  content_style: `
+    body {
+      background-color: #1D1E26;
+      color: #E6E6EB;
+    }
+  ` ,
 
+  plugins: ['lists', 'code'],
+  toolbar: 'bold italic | bullist numlist | code',
+
+  menubar: false,
   branding: false
 });
+
+
+// function test(){
+//   const bitch = document.querySelector(".tox tox-tinymce")
+//   // const havij = document.getElementById("tinymce")
+//   // const parent = havij.parentElement
+//   bitch.style.backgroundColor = "black"
+// }
+// window.onload = test
+
+
  /* 
    plugins: [
     'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
@@ -98,11 +114,11 @@ tinymce.init({
 
  */
 
-window.onload = function(){
-    const ad = document.querySelector(".tox-promotion-link");
-    const divWithAd = document.querySelector(".tox-promotion");
-    const otherBs = this.document.querySelector(".tox-menubar");
-    ad.remove()
-    divWithAd.remove()
-    otherBs.remove()
-}
+// window.onload = function(){
+//     const ad = document.querySelector(".tox-promotion-link");
+//     const divWithAd = document.querySelector(".tox-promotion");
+//     const otherBs = this.document.querySelector(".tox-menubar");
+//     ad.remove()
+//     divWithAd.remove()
+//     otherBs.remove()
+// }
